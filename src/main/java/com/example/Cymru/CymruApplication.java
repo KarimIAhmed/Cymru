887bc3df5ed618bdeb19da59efc0b967ae11856a
Karim Ahmed <karimib123@hotmail.com>
2022-09-23 19:27:43 -0400

    Commit

---
+++ src/main/java/com/example/Cymru/CymruApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cymru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CymruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CymruApplication.class, args);
+	}
+
+}
